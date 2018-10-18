62bc42aee487fcb82515295c807d4a25c145ecc1
nastya <askasoba16@ukr.net>
2018-10-18 15:26:47 +0300

    commit

---
+++ Program/ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public bool Number(int a)
+        {
+            if (a < 2) return false;
+            if (a%2 == 0) return false;
+            return true;
+        }
+
+
+        public  int Fibon(int x)
+        {
+            if (x == 1) { return 1; }
+
+            if (x == 0) { return 0; }
+
+            else
+            {
+                return Fibon(x - 1) + Fibon(x - 2);
+            }
+        }
+    }
+}
---
+++ Program/Program/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Program
+{
+    using ClassLibrary1;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            Console.WriteLine("Enter 1 to create Fibbonachi/ 2 to check the number");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a == 1)
+            {
+                for (int i = 0; i < 20; i++)
+                {
+
+                    Console.WriteLine(class1.Fibon(i));
+
+
+                }
+                Console.ReadLine();
+            }
+            else if (a == 2)
+            {
+                for (int d = 0; d < 5; d++)
+                {
+                    Console.WriteLine("Enter the number");
+                    int b = int.Parse(Console.ReadLine());
+                    if (class1.Number(b))
+                    {
+                        Console.WriteLine("Is prime");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Is not prime");
+                    }
+                }Console.ReadLine();
+            }
+              
+              
+        }
+    }
+}
